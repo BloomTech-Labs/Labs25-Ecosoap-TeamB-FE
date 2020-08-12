b6b3bf2c1e86fd5fadccf06dd264fe1ab8d9d689
Ben Haus <ben@little-haus.com>
2020-08-12 15:41:48 -0400

    main page layout started with sidebar nav and ant.design colors adjusted to ESB

--- src/components/pages/Home/RenderHomePage.js
+++ src/components/pages/Home/RenderHomePage.js
@@ -1,11 +1,55 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Button } from 'antd';
+import { Layout, Menu, Button } from 'antd';
+
+// ant.design icons
+import {
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from '@ant-design/icons';
 
 function RenderHomePage(props) {
+  // constants to support ant.design
+  const { Header, Footer, Sider, Content } = Layout;
+
   const { userInfo, authService } = props;
+
   return (
     <div>
+      <Layout>
+        <Sider
+          breakpoint="lg"
+          collapsedWidth="0"
+          onBreakpoint={broken => {
+            console.log(broken);
+          }}
+          onCollapse={(collapsed, type) => {
+            console.log(collapsed, type);
+          }}
+        >
+          <div className="logo" />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+            <Menu.Item key="1" icon={<UserOutlined />}>
+              nav 1
+            </Menu.Item>
+            <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+              nav 2
+            </Menu.Item>
+            <Menu.Item key="3" icon={<UploadOutlined />}>
+              nav 3
+            </Menu.Item>
+            <Menu.Item key="4" icon={<UserOutlined />}>
+              nav 4
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header>Header</Header>
+          <Content>Content</Content>
+          <Footer>Footer</Footer>
+        </Layout>
+      </Layout>
       <h1>Hi {userInfo.name} Welcome to Labs Basic SPA</h1>
       <div>
         <p>
--- src/styles/theme-overrides.js
+++ src/styles/theme-overrides.js
@@ -1,5 +1,5 @@
 module.exports = {
-  '@primary-color': '#ec3944',
+  '@primary-color': '#3BB54A',
   '@link-color': ' #ec3944',
   '@success-color': '#52c41a',
   '@warning-color': '#faad14',
@@ -13,4 +13,8 @@ module.exports = {
   '@border-color-base': '#d9d9d9',
   '@box-shadow-base':
     '0 3px 6px -4px rgba(0, 0, 0, 0.12), 0 6px 16px 0 rgba(0, 0, 0, 0.08), 0 9px 28px 8px rgba(0, 0, 0, 0.05)',
+  '@layout-header-background': '@layout-body-background',
+  '@layout-sider - background': '#FFFFFF',
+  '@menu-item-active-bg': '@primary-color',
+  '@menu-item - color': '@text-color',
 };
