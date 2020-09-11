3e2f6cb814c4559d099704e6074b92f4436a9b32
VladyslavHorbachenko <vladgorbachenko1234@gmail.com>
2020-09-11 14:23:31 -0700

    new styles

--- src/components/pages/ExampleList/ExampleListContainer.js
+++ src/components/pages/ExampleList/ExampleListContainer.js
@@ -1,22 +1,76 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Layout, Typography } from 'antd';
 import './Map.css';
 
-import { getExampleData } from '../../../api';
+import { Sidebar } from '../../common';
+import { TypesList } from '../../DataTypeTable';
 
-import { List } from '../../common';
-import RenderExampleListPage from './RenderExampleListPage';
+const ExampleList = props => {
+  const { Header, Footer, Content } = Layout;
+  const { Title } = Typography;
 
-// Here is an example of using our reusable List component to display some list data to the UI.
-const ExampleList = () => {
   return (
     <div>
-      <h1>Kaban</h1>
-      <iframe
-        src="https://api.mapbox.com/styles/v1/vlahorba/ckeyop2vy09aq19mvsb38yg2v.html?fresh=true&title=view&access_token=pk.eyJ1IjoidmxhaG9yYmEiLCJhIjoiY2s4ZjQ0d20yMDBlYTNscW9lZG0zNXU5ayJ9.ZQKJxY3FUYlxPyCuOqOxMA"
-        frameborder="0"
-      ></iframe>
+      <>
+        <Layout style={{ minHeight: '100vh' }}>
+          <Sidebar />
+          <Layout>
+            <Header>
+              <iframe
+                src="https://api.mapbox.com/styles/v1/vlahorba/ckeyop2vy09aq19mvsb38yg2v.html?fresh=true&title=view&access_token=pk.eyJ1IjoidmxhaG9yYmEiLCJhIjoiY2s4ZjQ0d20yMDBlYTNscW9lZG0zNXU5ayJ9.ZQKJxY3FUYlxPyCuOqOxMA"
+                frameborder="0"
+              ></iframe>
+            </Header>
+            <Content></Content>
+            <Footer>Footer</Footer>
+          </Layout>
+        </Layout>
+      </>
     </div>
   );
 };
 
 export default ExampleList;
+
+//TODO: look into udating this propTypes code
+
+// Don't forget your prop types! It will save you a lot of debugging headache as you add more features.
+ExampleList.propTypes = {
+  data: PropTypes.arrayOf(
+    // Here is an example of enforcing an object structure that we expect to receive in our props:
+    PropTypes.shape({
+      // Here we require an id of type number or string to prevent a "unique key prop" warning
+      id: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
+      title: PropTypes.string,
+      url: PropTypes.string,
+      thumbnailUrl: PropTypes.string,
+    })
+  ).isRequired,
+};
+
+// import React from 'react';
+// import './Map.css';
+// import { Sidebar } from '../../common';
+// import { TypesList } from '../../DataTypeTable';
+//
+// import { getExampleData } from '../../../api';
+//
+// import { List } from '../../common';
+// import RenderExampleListPage from './RenderExampleListPage';
+//
+// // Here is an example of using our reusable List component to display some list data to the UI.
+// const ExampleList = () => {
+//   return (
+//     <div>
+//       <h1>Kaban</h1>
+//
+//       <iframe
+//         src="https://api.mapbox.com/styles/v1/vlahorba/ckeyop2vy09aq19mvsb38yg2v.html?fresh=true&title=view&access_token=pk.eyJ1IjoidmxhaG9yYmEiLCJhIjoiY2s4ZjQ0d20yMDBlYTNscW9lZG0zNXU5ayJ9.ZQKJxY3FUYlxPyCuOqOxMA"
+//         frameborder="0"
+//       ></iframe>
+//     </div>
+//   );
+// };
+//
+// export default ExampleList;
